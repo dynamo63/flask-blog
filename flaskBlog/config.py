5907ab95fb84bccab515582a490ef1a1c09af801
dynamo63 <smoulongui@gmail.com>
2020-05-23 00:23:18 +0100

    Add python-dotenv for variable environnement

--- flaskBlog/config.py
+++ flaskBlog/config.py
@@ -2,13 +2,13 @@ import os
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
-    SECRET_KEY = 'CnBIZtOBkzpDYkIWUJ44DuLfDe8PAOO4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class Development(BaseConfig):
     DEBUG = True
     ENV = 'dev'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dynamo74:sabil2000@localhost/flaskBlogData'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class Production(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
